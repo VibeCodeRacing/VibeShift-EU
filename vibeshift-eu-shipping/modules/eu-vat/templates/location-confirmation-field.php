e5f79357ffe574178e0a04f6a783ae68bdf4179a
Doug Wagner <doug@dougstate.com>
2026-07-13 18:31:02 -0400

    Fix i18n translators comment above location confirmation string

--- vibeshift-eu-shipping/modules/eu-vat/templates/location-confirmation-field.php
+++ vibeshift-eu-shipping/modules/eu-vat/templates/location-confirmation-field.php
@@ -14,9 +14,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 <p class="form-row location_confirmation terms">
 	<label for="location_confirmation" class="checkbox"><input type="checkbox" class="input-checkbox" name="location_confirmation" <?php checked( $location_confirmation_is_checked, true ); ?> id="location_confirmation" /> <span>
 	<?php
-		/* translators: %s billing country */
 		echo wp_kses_post(
 			sprintf(
+				/* translators: %s: billing country name */
 				__( 'I am established, have my permanent address, or usually reside within <strong>%s</strong>.', 'vibeshift-eu-shipping' ),
 				$countries[ is_callable( array( WC()->customer, 'get_billing_country' ) ) ? WC()->customer->get_billing_country() : WC()->customer->get_country() ]
 			)
