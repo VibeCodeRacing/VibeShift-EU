21797369c6eef126aa94de718eb46138ae137527
Doug Wagner <doug@dougstate.com>
2026-07-13 18:08:08 -0400

    Silence Plugin Check false positives on settings helper and template

--- vibeshift-eu-shipping/modules/eu-vat/templates/location-confirmation-field.php
+++ vibeshift-eu-shipping/modules/eu-vat/templates/location-confirmation-field.php
@@ -14,10 +14,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 <p class="form-row location_confirmation terms">
 	<label for="location_confirmation" class="checkbox"><input type="checkbox" class="input-checkbox" name="location_confirmation" <?php checked( $location_confirmation_is_checked, true ); ?> id="location_confirmation" /> <span>
 	<?php
-		$vibeshift_eu_shipping_billing_country = is_callable( array( WC()->customer, 'get_billing_country' ) ) ? WC()->customer->get_billing_country() : WC()->customer->get_country();
 		/* translators: %s billing country */
-		echo wp_kses_post( sprintf( __( 'I am established, have my permanent address, or usually reside within <strong>%s</strong>.', 'vibeshift-eu-shipping' ), $countries[ $vibeshift_eu_shipping_billing_country ] ) );
-	?>
+		echo wp_kses_post(
+			sprintf(
+				__( 'I am established, have my permanent address, or usually reside within <strong>%s</strong>.', 'vibeshift-eu-shipping' ),
+				$countries[ is_callable( array( WC()->customer, 'get_billing_country' ) ) ? WC()->customer->get_billing_country() : WC()->customer->get_country() ]
+			)
+		);
+		?>
 	</span>
 	</label>
 </p>
